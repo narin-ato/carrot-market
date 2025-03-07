65b8f0057835facbe6a3c8c2e25829187f853d95
narin-ato <narinato.bro@gmail.com>
2025-03-07 17:58:02 +0900

    Updated to #3.7

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,34 @@
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 rounded-2xl w-full max-w-screen-sm dark:bg-gray-600">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="text-4xl font-semibold dark:text-white">
-              Coolblue
-            </span>
+    <main className="bg-gray-300 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col">
+        {["Nico", "Me", "You"].map((person, index) => (
+          <div key={index} className="flex items-center gap-5">
+            <div />
+            <span>{person}</span>
+            <div>
+              <span>{index}</span>
+            </div>
           </div>
-          <div className="size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full transition hover:bg-green-500 hover:scale-125">
-            Tdoay
-          </span>
-          <span className="dark:text-gray-100">9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 rounded-full absolute w-full h-2" />
-          <div className="bg-green-400 rounded-full absolute w-2/3 h-2" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+        ))}
       </div>
     </main>
+
+    // <main className="bg-gray-300 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+    //   <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none *:md:text-red-100 has-[:invalid]:ring-red-100 has-[:invalid]:ring ring ring-transparent transition-shadow">
+    //     <input
+    //       className="w-full rounded-full h-10 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow  invalid:focus:ring-red-500 peer"
+    //       type="text"
+    //       placeholder="Email address"
+    //       required
+    //     />
+    //     <span className="text-red-500 font-medium hidden peer-invalid:block">
+    //       Email is required
+    //     </span>
+    //     <button className="text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium md:px-10 bg-black peer-[:invalid]:bg-red-200">
+    //       Log in
+    //     </button>
+    //   </div>
+    // </main>
   );
 }
