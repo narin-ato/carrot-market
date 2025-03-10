05df059d6f70f2094a3de6cf8825b0379c0a0614
narin-ato <narinato.bro@gmail.com>
2025-03-10 21:31:52 +0900

    Updated to #5.3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,12 @@
-"use client";
-
 import FormInput from "@/components/form-input";
 import FormButton from "@/components/form-btn";
 import SocialLogin from "@/components/\bsocial-login";
 
 export default function LogIn() {
-  const onClick = async () => {
-    const response = await fetch("/www/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "nico",
-        password: 123,
-      }),
-    });
-    console.log(await response.json())
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    console.log("logged in!");
   };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -21,18 +14,23 @@ export default function LogIn() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
           errors={[]}
         />
+        <FormButton text="Log in" />
       </form>
-      <span onClick={onClick}>
-        <FormButton loading={false} text="Log in" />
-      </span>
       <SocialLogin />
     </div>
   );
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -1,22 +1,32 @@
 interface FormInputProps {
   type: string;
-  placeholder:string;
-  required:boolean;
-  errors:string[]
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+  name: string;
 }
 
-export default function FormInput({type, placeholder, required, errors}:FormInputProps) {
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+  name,
+}: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
-          <input
-            className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-neutral-200 focus:ring-orange-500 ring-1 focus:ring-2 px-3 placeholder:text-neutral-400"
-            type={type}
-            placeholder={placeholder}
-            required={required}
-          />
-          {errors.map((error, index) => (
-            <span key={index} className="text-red-500 font-medium">{error}</span>
-          ))}
-        </div>
+      <input
+        name={name}
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-neutral-200 focus:ring-orange-500 ring-1 focus:ring-2 px-3 placeholder:text-neutral-400"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      {errors.map((error, index) => (
+        <span key={index} className="text-red-500 font-medium">
+          {error}
+        </span>
+      ))}
+    </div>
   );
 }
